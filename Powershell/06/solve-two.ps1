999c74e91d4746e7e5dac3f6d497933d3bb3d263
Arthur_Familie_Laptop <isaak.a@outlook.com>
2025-01-01 00:23:00 +0100

    Solved D06 P02 when brute foce works

--- Powershell/06/solve-two.ps1
+++ Powershell/06/solve-two.ps1
@@ -42,27 +42,86 @@ function get-input(){
 ## Task specific functions ##
 #############################
 
-function map-rout(){
-    $thisGrid=@($script:guardStart[0],$script:guardStart[1])
-    $stepCount = 0
-    while($thisGrid[0] -ge 0 -and $thisGrid[1] -ge 0 -and $thisGrid[0] -lt $script:rowCount -and $thisGrid[1] -lt $script:colCount){
-        Write-Progress -Activity "Mapping guard rout" -Status "Steps taken ${stepCount}"
-        #log in vector
-        $inVector = $script:direction
-        #get next
-        $nextGrid=get-nextGrid -Grid $thisGrid
-        if(is-blocked -Grid $nextGrid){
-            # if next block
-            switch-direction
-            $nextGrid = get-nextGrid -Grid $thisGrid
-        }
-        #log out vector
-        $outVector = $script:direction
-        # add to rout 
-        $thisStep=[PSCustomObject]@{vectorIn=$inVector; vectorOut=$outVector; grid=$thisGrid; index = $stepCount}
-        $script:guardPath.Add($thisStep)
-        $thisGrid=$nextGrid
-        $stepCount++
+function add-Block(){
+    param(
+        [System.Array]$Grid
+    )
+
+    if(-not $script:blocks.ContainsKey($Grid[0])){
+        $script:blocks[$Grid[0]]=@{}
+    }
+    $script:blocks[$Grid[0]][$Grid[1]]=@($Grid[0],$Grid[1])
+}
+
+function remove-Block(){
+    param(
+        [System.Array]$Grid
+    )
+    $script:blocks[$Grid[0]].Remove($Grid[1])
+    if($script:blocks[$Grid[0]].Count -eq 0){$script:blocks.Remove($Grid[0])}
+}
+
+function get-key(){
+    param(
+        [System.Array]$Grid,
+        [string]$Direction
+    )
+    return "[$($Grid[0])|$($Grid[1])] $Direction"
+}
+
+function sim-loop(){
+    param(
+        [System.Array]$Grid,
+        [string]$Direction
+    )
+    
+    $thisKey = get-key -Grid $Grid -Direction $Direction
+    add-to-logline "TK ${thisKey}| "
+    $script:miniloop[$thisKey]=$Grid
+    $nextStop = get-nextStop -Grid $Grid -Direction $Direction
+    add-to-logline "NK $($nextStop.Key)| "
+    if($null -eq $nextStop -or $script:leadsToExit.ContainsKey($nextStop.Key)){
+        # if(-not $script:pathTraveld.ContainsKey($thisKey)){$script:leadsToExit[$thisKey]=$Grid}
+        add-to-logline "Null OR LeadsToExit. "
+        return $false
+    }elseif($script:pathTraveld.ContainsKey($nextStop.Key)){
+        add-to-logline "On traveld path. "
+        return $true
+    }elseif($script:miniloop.ContainsKey($nextStop.Key)){
+        add-to-logline "Stuck in miniloop. "
+        return $true
+    }elseif(sim-loop -Grid $nextStop.Grid -Direction $nextStop.Direction){
+        return $true
+    }else{
+        #if(-not $script:pathTraveld.ContainsKey($thisKey)){$script:leadsToExit[$thisKey]=$Grid}
+        return $false
+    }
+}
+
+function get-nextStop {
+    param (
+        [System.Array]$Grid,
+        [string]$Direction
+    )
+    $nextR=$Grid[0]
+    $nextC=$Grid[1]
+    switch ($Direction) {
+        "up"    {$nextR= $script:blocks.Keys|Where-Object{$_ -lt $nextR}|Sort-Object -Descending|
+                 Where-Object{$script:blocks[$_].Contains($nextC)}|Select-Object -First 1;
+                 if($null -eq $nextR){$nextR=-1}else{$nextR+=1};break }
+        "down"  {$nextR= $script:blocks.Keys|Where-Object{$_ -gt $nextR}|Sort-Object|
+                 Where-Object{$script:blocks[$_].Contains($nextC)}|Select-Object -First 1; $nextR-=1;break }
+        "left"  {$nextC= $script:blocks[$nextR].Keys|Where-Object {$_ -lt $nextC}|
+                 Sort-Object -Descending|Select-Object -First 1;if($null -eq $nextC){$nextC=-1}else{$nextC+=1};break }
+        "right" {$nextC= $script:blocks[$nextR].Keys|Where-Object {$_ -gt $nextC}|
+                 Sort-Object|Select-Object -First 1; $nextC-=1;break }
+    }
+    $newDirection=switch-direction -directionIn $Direction
+    $newKey=get-key -Grid @($nextR,$nextC) -Direction $newDirection
+    if($nextR -lt 0 -or $nextC -lt 0){
+        return $null
+    }else{
+        return @{Key=$newKey;Grid=@($nextR,$nextC);Direction=$newDirection} 
     }
 }
 
@@ -82,97 +141,17 @@ function get-nextGrid(){
     return @($r,$c)
 }
 
-function is-blocked(){
-    param(
-        [Parameter()]
-        [System.Array]$Grid
-    )
-    $next = $script:bloks | Where-Object {$_.row -eq $Grid[0] -and $_.col -eq $Grid[1]}
-    return ($null -ne $next)
-}
-
-function bogus-blocks(){
-    $stepCount = 0 ; $stepsToCheck = $script:guardPath.Count
-    foreach($gridSlot in $script:guardPath){
-        Write-Progress -Activity "Searching locations for bogus bloks" -Status "At gridslot [$($gridSlot.grid)]. Possible locations maped: ${script:result}" -PercentComplete (($stepCount/$stepsToCheck)*100)
-        if($gridSlot.grid[0] -ne $script:guardStart[0] -or $gridSlot.grid[1] -ne $script:guardStart[1]){
-             add-to-logline "Try at [$($gridSlot.grid)]"
-            $script:direction = $gridSlot.vectorIn
-             add-to-logline "direction in: ${script:direction}"
-            $incommig = get-incommig -Grid $gridSlot.grid
-             add-to-logline "incomming [$($incommig.grid)]"         
-            switch-direction
-             add-to-logline "direction out: ${script:direction}"
-            $nextStop = get-nextStop -Grid $incommig.grid
-             add-to-logline "next stop [$($nextStop.grid)]"
-            if($null -ne $nextStop -and $incommig.index -gt $nextStop.index){
-                add-to-logline " bogus block at ($($gridSlot.grid))" 
-                $script:result+=1
-            }
-        }
-        $stepCount++
-         log
-    }
-}
-
-function get-incommig(){
-    param(
-        [Parameter()]
-        [System.Array]$Grid,
-        [switch]$directionIn
-    )
-       $r=$Grid[0]
-       $c=$Grid[1]
-       switch($script:direction){
-            "up"    {$r+=1}
-            "down"  {$r-=1}
-            "left"  {$c+=1}
-            "right" {$c-=1}
-        }
-        $incomming = $script:guardPath | Where-Object {$_.grid[0] -eq $r -and 
-                                                       $_.grid[1] -eq $c -and 
-                                                       $(if($directionIn){
-                                                            $_.vectorIn -eq $script:direction
-                                                       }else{$_.vectorOut -eq $script:direction})}
-        return $incomming
-}
-
-function get-nextStop(){
-    param(
-        [Parameter()]
-        [System.Array]$Grid
-    )
-    #get next block
-    switch($script:direction){
-        "up"    {$nextBlock = $script:bloks | 
-                    Where-Object {$_.col -eq $Grid[1] -and $_.row -lt $Grid[0]} |
-                    Sort-Object -Descending -Property row |
-                    Select-Object -First 1;break}
-        "down"  {$nextBlock = $script:bloks | 
-                    Where-Object {$_.col -eq $Grid[1] -and $_.row -gt $Grid[0]} |
-                    Sort-Object -Property row |
-                    Select-Object -First 1;break}
-        "left"  {$nextBlock = $script:bloks | 
-                    Where-Object {$_.col -lt $Grid[1] -and $_.row -eq $Grid[0]} |
-                    Sort-Object -Descending -Property col |
-                    Select-Object -First 1;break}
-        "right" {$nextBlock = $script:bloks | 
-                    Where-Object {$_.col -gt $Grid[1] -and $_.row -eq $Grid[0]} |
-                    Sort-Object -Property col |
-                    Select-Object -First 1;break}
-    }
-    #get step from path
-    $incomming = get-incommig -Grid @($nextBlock.row, $nextBlock.col) -directionIn
-    return $incomming
-}
-
 function switch-direction(){
-    switch($script:direction){
-        "up"    {$script:direction = "right"; break}
-        "down"  {$script:direction = "left" ; break}
-        "left"  {$script:direction = "up"   ; break}
-        "right" {$script:direction = "down" ; break}
+    param(
+        [string]$directionIn
+    )
+    switch($directionIn){
+        "up"    {$directionOut = "right"; break}
+        "down"  {$directionOut = "left" ; break}
+        "left"  {$directionOut = "up"   ; break}
+        "right" {$directionOut = "down" ; break}
     }
+    return $directionOut
 }
 
 #################
@@ -184,16 +163,16 @@ $script:result = 0
 $script:puzle = get-input
 $rows = $script:puzle.Count
 $script:rowCount = 0
-$script:bloks= [System.Collections.Generic.List[PSCustomObject]]::New()
+$script:blocks=@{} 
 foreach($row in $script:puzle){
     $activity = "Loading puzle."
     $script:colCount = 0
+    $script:blocks[$script:rowCount]=@{}
     $row -split "" | Where-Object {"" -ne $_} | ForEach-Object{
         if($_ -eq "#"){
-            $script:bloks.Add([PSCustomObject]@{row=$script:rowCount;col=$script:colCount})
-            
+           add-Block -Grid @($script:rowCount, $script:colCount)
         }elseif($_ -eq "^"){
-            $script:guardStart=@($script:rowCount, $script:colCount)
+           $r= $script:rowCount; $c= $script:colCount
         }
         $script:colCount++
     }
@@ -201,8 +180,28 @@ foreach($row in $script:puzle){
     Write-Progress -Activity $activity -Status "Row: ${script:rowCount} of ${rows}" -PercentComplete $(($script:rowCount/$rows)*100)
 }
 $script:direction = "up"
-$script:guardPath=[System.Collections.Generic.List[PSCustomObject]]::new()
-map-rout
-bogus-blocks
+$script:pathTraveld = @{}
+$script:leadsToExit = @{}
+while($r -ge 0 -and $c -ge 0 -and $r -lt $script:rowCount -and $c -lt $script:colCount){
+    $thisGrid = @($r, $c)
+    $thisKey = get-key -Grid $thisGrid -Direction $script:direction
+    $nextGrid = get-nextGrid -Grid $thisGrid
+    if($script:blocks.ContainsKey($nextGrid[0]) -and $script:blocks[$nextGrid[0]].ContainsKey($nextGrid[1])) {
+        $script:direction = switch-direction -DirectionIn $script:direction
+    }else{
+        add-to-logline "From ${thisKey}| "
+        $script:miniloop=@{}
+        if($script:leadsToExit.ContainsKey($thisKey)){$script:leadsToExit.Remove($thisKey)}
+        $script:pathTraveld[$thisKey]=$thisGrid
+        Write-Progress -Activity "Running Sims" -Status "From ${thisKey}"
+        add-Block -Grid $nextGrid
+        $script:result+=$(if((sim-loop -Grid $thisGrid -Direction $script:direction)){1}else{0})
+        $r=$nextGrid[0]
+        $c=$nextGrid[1]
+        remove-Block -Grid $nextGrid
+        log
+    }
+}
+
 $stopwatch.Stop()
 Write-Host "Result ${script:result} `nTotal runtime: $($stopwatch.Elapsed.TotalMinutes) minutes"
