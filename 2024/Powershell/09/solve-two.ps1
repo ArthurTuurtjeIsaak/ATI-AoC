a2b2fc091de854a7f2b918e2aa3a546140944104
Arthur_Familie_Laptop <isaak.a@outlook.com>
2025-01-19 17:34:38 +0100

    D09 P02 Runtime down to 19 min in ISE wrong anwser still

--- 2024/Powershell/09/solve-two.ps1
+++ 2024/Powershell/09/solve-two.ps1
@@ -44,6 +44,47 @@ function get-input(){
 #############################
 ## Task specific functions ##
 #############################
+function Get-MatchingSectorKey {
+    param(
+        [int]$Length,
+        [int]$Index
+    )
+   $sectorKey = $emptySectors.Keys | Where-Object {$emptySectors[$_].emptySpace -ge $Length} | Sort-Object | Select-Object -First 1
+   $highKey = $emptySectors.Keys | Sort-Object -Descending| Select-Object -First 1
+   if($null -eq $sectorKey){
+       log "-----------------------------------" 
+       log "Mapping empty sectors from ${highKey} to ${Index}."
+        for([int]$key = ($highKey+2); $key -lt $Index; $key+=2){
+            add-to-logLine "${key}|"
+            $emptySectors.Add($key, [PSCustomObject]@{totalSpace=[int][string]$row[$key]; emptySpace=[int][string]$row[$key]})
+            $emptySectors[$key] | Add-Member -MemberType ScriptMethod -Name innerIndex -Value $innerIndex
+            if($emptySectors[$key].emptySpace -ge $Length){
+                $sectorKey = $key
+                break
+            }
+        }
+        log
+        log "----------------------------------"
+   }
+    return $sectorKey
+}
+
+function Get-Position{
+    param(
+        [int]$Index
+    )
+    if(-not $positions.containsKey($Index)){
+        $highKey = $positions.Keys | Sort-Object -Descending | Select-Object -First 1
+        log "----------------------------------"
+        log "Mapping pos from ${highKey} to ${Index}"
+        for($j=($highKey+1); $j -le $Index; $j++){
+            $positions.Add($j, $positions[($j-1)]+[int][string]$row[($j-1)])
+        }
+        log "----------------------------------"
+    }
+    return $positions[$Index]
+}
+
 $innerIndex = {
     return ($this.totalSpace - $this.emptySpace)
 }
@@ -54,57 +95,55 @@ $innerIndex = {
 $stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
 initiate
 $script:puzle = get-input
-$files=@{}
-$emptySectors=@{} 
-$positions=@{}
-$index=0
-$rowPos=0
-$script:puzle -Split "" | Where-Object {$_ -ne ""}| ForEach-Object {
-    Write-Progress -Activity "Mapping the puzzle"  -PercentComplete (($index/$script:puzle.Length)*100)
-    if(($index % 2) -eq 0){
-        $files.Add($index, [PSCustomObject]@{length=$_; optimized=$false; id=($index / 2)})
-    }else{
-        $emptySectors.Add($index, [PSCustomObject]@{totalSpace=[int]$_; emptySpace=[int]$_})
-        $emptySectors[$index] | Add-Member -MemberType ScriptMethod -Name innerIndex -Value $innerIndex
-    }
-    $rowPos+=[int]$_
-    $positions.Add(++$index, $rowPos)
-} 
 
 #  0     1     2     3     4
-#  F  s  F  s  F  s  F  s  F
-#  2  3  3  3  1  3  3  1  2
-#  0  1  2  3  4  5  6  7  8
+#  0  2  5  8  11 12 15 18 19 21 22 26 27 31 32 35 36 40 40
+#  2  3  3  3  1  3  3  1  2  1  4  1  4  1  3  1  4  0  2
+#  0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18
 
-[int]$fi= $files.Keys | Sort-Object -Descending | Select-Object -First 1
-# defrag and cheksum from back to front
-for($fi; $fi -gt 0; $fi-=2){
-    Write-Progress -Activity "Optimizing files" -Status "Checking file $($files[$fi].id) at ${fi}"
-    if(-not $files[$fi].optimized){
-        # find empty space
-        $esi = $emptySectors.Keys | Where-Object {$emptySectors[$_].emptySpace -ge $files[$fi].length} | Sort-Object | Select-Object -First 1
-        if($null -ne $esi){
-            # get pos 
-            $pos = ($positions[$esi] + $emptySectors[$esi].innerIndex())
-            # fill empty space
-            $emptySectors[$esi].emptySpace = $emptySectors[$esi].emptySpace - $files[$fi].length
-            # checksum
-            for([int]$i=0; $i -lt $files[$fi].length; $i++){
-                $checkSum = $files[$fi].id * ($pos+$i)
-                $checkSums+=$checkSum
-            }
-            Remove-Variable i
-            $files[$fi].optimized = $true
+$row = $script:puzle # omdat ik geen zin heb om steeds $script:puzle te typen
+$endOfRow = $row.Length
+log "Input length = ${endOfRow}"
+log "======================================================================================="
+$emptySectors=@{} 
+# preload first empty sector
+$emptySectors.Add(1, [PSCustomObject]@{totalSpace=[int][string]$row[1]; emptySpace=[int][string]$row[1]})
+$emptySectors[1] | Add-Member -MemberType ScriptMethod -Name innerIndex -Value $innerIndex
+
+$positions=@{}
+# preload first position
+$positions.Add(1, [int][string]$row[0])
+$checkSums=0
+for([int]$i=($endOfRow-1); $i -gt 0; $i--){
+    Write-Progress -Activity "Defragmentation in process" -Status "Defrag at index ${i}" -PercentComplete ((($endOfRow-$i)/$endOfRow)*100)
+    # if file
+    if(($i % 2)-eq 0){
+        $fileId = $i / 2
+        $fileLength = [int][string]$row[$i]
+        log "File length: $($row[$i]), at ${i}, id ${fileId}"
+        
+        # find empty sector that fits
+        $msKey = Get-MatchingSectorKey -Length $fileLength -Index $i
+        $pos = 0
+        if($null -ne $msKey){
+            log "Empty sector found at ${msKey}, inner index: $($emptySectors[$msKey].innerIndex()), empty space: $($emptySectors[$msKey].emptySpace), total space: $($emptySectors[$msKey].totalSpace)"
+            $pos = Get-Position -Index $msKey
+            log "position at ${msKey}: ${pos}"
+            $pos = $pos + $emptySectors[$msKey].innerIndex()
+            $emptySectors[$msKey].emptySpace = $emptySectors[$msKey].emptySpace - $fileLength
+        }else{
+            log "No empty sector found."
+            $pos = Get-Position -Index $i
+            log "position at ${i}: ${pos}"
         }
-    }
-}
-# checksum others
-$files.Keys | Where-Object {-not $files[$_].optimized} | ForEach-Object {
-    Write-Progress -Activity "Checksum whats left" -Status "file $($files[$_].id) at $($_)."
-    $pos = $positions[$_]
-    for([int]$i=0; $i -lt $files[$_].length; $i++){
-        $checkSum = $files[$_].id * ($pos+$i)
-        $checkSums+=$checkSum
+        for($ii=0; $ii -lt $fileLength; $ii++){
+            $checkSum = $fileId * ($pos+$ii)
+            log "Checksum: ${fileId} * $(($pos+$ii)) = ${checkSum}"
+            add-to-logline "${checkSums} + ${checkSum} = "
+            $checkSums = $checkSums+$checkSum
+            add-to-logline "${checkSums}" ; log
+        }
+        log "======================================================================================="
     }
 }
 Write-Host "Result: ${checkSums}"
